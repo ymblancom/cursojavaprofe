5afd23dd080e6dd8a34fffeabe8cf3fe9712d531
Tardes <Tardes@SB11-00>
2024-09-19 18:33:39 +0200

    Añadido variable apellidos

--- src/com/curso/java/PrimeraClase.java
+++ src/com/curso/java/PrimeraClase.java
@@ -5,7 +5,8 @@ public class PrimeraClase {
 	public static void main(String[] args) {
 		
 		
-		String nombre = "Jose Luis"; 
+		String nombre = "Jose Luis";
+		String apellidos = "Llorente";
 		System.out.println(nombre);
 		
 		
