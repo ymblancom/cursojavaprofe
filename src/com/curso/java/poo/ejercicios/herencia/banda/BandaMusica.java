eaaf41a29d6b2e3cb3f5253407acabb1437c141c
Tardes <Tardes@SB0300>
2024-09-25 20:44:21 +0200

    Ejercicio Banda Música clase con main()

--- src/com/curso/java/poo/ejercicios/herencia/banda/BandaMusica.java
+++ src/com/curso/java/poo/ejercicios/herencia/banda/BandaMusica.java
@@ -3,8 +3,35 @@ package com.curso.java.poo.ejercicios.herencia.banda;
 public class BandaMusica {
 
 	public static void main(String[] args) {
-		//Instrumento instrumento = new Instrumento("I1","t1");
-
+		BandaMusica banda = new BandaMusica();
+		banda.empezarConcierto();
 	}
 
+	private void empezarConcierto() {
+		Instrumento[] instrumentos = crearInstrumentos();
+		afinarInstrumentos(instrumentos);
+	}
+	
+	private Instrumento[] crearInstrumentos() {
+		System.out.println("Creando instrumentos");
+		Guitarra guitarra = new Guitarra("Guitarra1","Tipo");
+		Guitarra guitarraElectrica = new GuitarraElectrica("Guitarra1","Tipo",false,150);
+		Piano piano = new Piano("Paino1", "Cuerda", false, 7, "De cola");
+		Tambor tambor = new Tambor("TAmbor1","",false,"Ojalata");
+		
+		Instrumento[] instrumentos = {guitarra, guitarraElectrica, piano, tambor};
+		System.out.println("Creados  "+ instrumentos.length + " instrumentos");
+		return instrumentos;
+	}
+	
+	private void afinarInstrumentos(Instrumento[] instrumentos) {
+		System.out.println("Afinando instrumentos");
+		for (Instrumento instrumento : instrumentos) {
+			instrumento.limpiar();
+			instrumento.afinar();
+		}
+		System.out.println("Instrumentos Afinados");
+	}
+	
 }
+
